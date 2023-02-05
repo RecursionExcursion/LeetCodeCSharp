e94cb7b7ddddc887210584bb2aac6bc1f6514a4f
RecursionExcursion <rloup15@gmail.com>
2023-02-04 20:18:17 -0500

    1 Problem, DetermineColorOfChessboardSquare

--- LeetCodeCSharp/Program.cs
+++ LeetCodeCSharp/Program.cs
@@ -10,17 +10,12 @@ namespace LeetCodeCSharp
         static void Main(string[] args)
         {
 
-            string s = "codeleet";
-            int[] indices = { 4, 5, 6, 7, 0, 2, 1, 3 };
+          
 
 
-            string s2 = "abc";
-            int[] indices2 = { 0, 1, 2 };
-
-
-        Console.WriteLine(ShuffleString.RestoreString(s,indices));
-        Console.WriteLine(ShuffleString.RestoreString(s2,indices2));
-
+        Console.WriteLine(DetermineColorOfAChessboardSquare.SquareIsWhite("a1"));
+        Console.WriteLine(DetermineColorOfAChessboardSquare.SquareIsWhite("h3"));
+        Console.WriteLine(DetermineColorOfAChessboardSquare.SquareIsWhite("c7"));
         }
     }
 }
---
+++ LeetCodeCSharp/problems/DetermineColorOfAChessboardSquare.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp.problems
+{
+    public class DetermineColorOfAChessboardSquare
+    {
+        public static bool SquareIsWhite(string coordinates)
+        {
+
+            byte[] asciBytes = Encoding.ASCII.GetBytes(coordinates);
+            return asciBytes[0] % 2 != 0 ? asciBytes[1] % 2 == 0 : asciBytes[1] % 2 != 0;
+        }
+    }
+}
