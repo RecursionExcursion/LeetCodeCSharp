b04d19005744d4aa03e3360aa5ad8223226be396
RecursionExcursion <rloup15@gmail.com>
2023-02-07 12:15:05 -0500

    1 problem, CountDaysSpentTogether

--- LeetCodeCSharp/Program.cs
+++ LeetCodeCSharp/Program.cs
@@ -9,8 +9,8 @@ namespace LeetCodeCSharp
     {
         static void Main(string[] args)
         {
-        Console.WriteLine(DivideArrayIntoEqualPairs.DivideArray(new int[] {3,2,3,2,2,2}));
-        Console.WriteLine(DivideArrayIntoEqualPairs.DivideArray(new int[] {1,2,3,4}));
+        Console.WriteLine(CountDaysSpentTogether.CountDaysTogether("08-15","08-18","08-16","08-19"));
+        Console.WriteLine(CountDaysSpentTogether.CountDaysTogether("10-01","10-31","11-01","12-31"));
         }
     }
 }
---
+++ LeetCodeCSharp/problems/CountDaysSpentTogether.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp.problems
+{
+    public class CountDaysSpentTogether
+    {
+        public static int CountDaysTogether(String arriveAlice, String leaveAlice, String arriveBob, String leaveBob)
+        {
+            DateTime StringToDate(String date)
+            {
+                string[] ArrivalString = date.Split("-");
+                int[] Arrival = new int[ArrivalString.Length];
+
+                for (int i = 0; i < ArrivalString.Length; i++)
+                {
+                    Arrival[i] = int.Parse(ArrivalString[i]);
+                }
+                return new DateTime(1999, Arrival[0], Arrival[1]);
+            }
+
+            DateTime aliceArrivalDate = StringToDate(arriveAlice);
+            DateTime aliceDepartDate = StringToDate(leaveAlice);
+
+            DateTime bobArrivalDate = StringToDate(arriveBob);
+            DateTime bobDepartDate = StringToDate(leaveBob);
+
+            DateTime secondArr = bobArrivalDate < aliceArrivalDate ? aliceArrivalDate : bobArrivalDate;
+            DateTime firstDep = bobDepartDate < aliceDepartDate ? bobDepartDate : aliceDepartDate;
+            int ans = (int)(firstDep - secondArr).TotalDays + 1;
+            return ans > 0 ? ans : 0;
+
+        }
+    }
+}
