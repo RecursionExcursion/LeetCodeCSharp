be4000638a05086a3de426bbae4dbd27452e5964
RecursionExcursion <rloup15@gmail.com>
2023-12-07 17:30:45 -0500

    12/7/23

---
+++ LeetCodeCSharp/problems/CheckIfEveryRowAndColumnContainsAllNumbers.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp.problems
+{
+    public class CheckIfEveryRowAndColumnContainsAllNumbers
+    {
+        public bool CheckValid(int[][] matrix)
+        {
+
+            int l = matrix.Length;
+
+            for (int i = 0; i < l; i++)
+            {
+                bool[] n1 = new bool[l];
+                bool[] n2 = new bool[l];
+
+                for (int j = 0; j < l; j++)
+                {
+                    if (n1[matrix[i][j] - 1] == true || n2[matrix[j][i] - 1] == true)
+                    {
+                        return false;
+                    }
+                    n1[matrix[i][j] - 1] = true;
+                    n2[matrix[j][i] - 1] = true;
+                }
+            }
+            return true;
+        }
+        //public bool CheckValid(int[][] matrix)
+        //{
+
+
+        //    int l = matrix.Length;
+        //    bool[] nums;
+
+        //    foreach (var row in matrix)
+        //    {
+        //        nums = new bool[l];
+
+        //        foreach (var cell in row)
+        //            nums[cell - 1] = true;
+
+        //        foreach (var n in nums)
+        //            if (!n)
+        //                return false;
+        //    }
+
+
+        //    for (int x = 0; x < l; x++)
+        //    {
+        //        nums = new bool[l];
+
+        //        for (int y = 0; y < l; y++)
+        //            nums[matrix[y][x] - 1] = true;
+
+        //        foreach (var n in nums)
+        //            if (!n)
+        //                return false;
+        //    }
+        //    return true;
+        //}
+    }
+}
---
+++ LeetCodeUnitTests/Tests/CheckIfEveryRowAndColumnContainsAllNumbersTest.cs
@@ -0,0 +1,31 @@
+﻿using LeetCodeCSharp.problems;
+
+namespace LeetCodeUnitTests.Tests
+{
+    internal class CheckIfEveryRowAndColumnContainsAllNumbersTest
+    {
+
+        private CheckIfEveryRowAndColumnContainsAllNumbers obj = new();
+
+        private static readonly int[][] case1 = new int[][] {
+            new int[] { 1, 2, 3 },
+            new int[] { 3, 1, 2 },
+            new int[] { 2, 3, 1 }
+        };
+
+        private static readonly int[][] case2 = new int[][] {
+            new int[] { 1,1,1 },
+            new int[] { 1,2,3},
+            new int[] { 1, 2, 3 }
+        };
+
+        [Test]
+        public void Test1() => Assert.That(obj.CheckValid(case1));
+        [Test]
+        public void Test2() => Assert.That(obj.CheckValid(case2), Is.False);
+
+
+
+
+    }
+}
